efdc1b7d325966b0fa2f422ac37374ef8be74d82
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-09-01 11:18:34 -0500

    pruebas server

--- src/test/java/com/server/ServerTest.java
+++ src/test/java/com/server/ServerTest.java
@@ -17,7 +17,7 @@ public class ServerTest {
     @BeforeClass
     public static void startServer(){
         System.out.println("Start");
-        Server.port(getPort());
+        Server.port(443);
         Server.staticFiles("static");
     }
 
